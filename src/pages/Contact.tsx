a83b47ec15ad47222b1fd576d8ce1d7b1ead1da0
anthonynomil <anthony.nomil@gmail.com>
2023-06-27 10:24:11 +0200

    Joe

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,130 +1,121 @@
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  Paper,
-  TextField,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import {Box, Button, Container, FormControl, Paper, TextField, Toolbar, Typography,} from "@mui/material";
 import * as Yup from "yup";
-import { useSnackbar } from "notistack";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+import {useSnackbar} from "notistack";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
 import * as emailJs from "@emailjs/browser";
-import { publicKey, serviceId, templateId } from "../config/emailjs.config";
+import {publicKey, serviceId, templateId} from "../config/emailjs.config";
 
 const Contact = () => {
-  const { enqueueSnackbar } = useSnackbar();
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email(`L'adresse email est invalide`)
-      .required(`L'adresse email est requise`),
-    subject: Yup.string().required(`Le sujet est requis`),
-    message: Yup.string().required(`Le message est requis`),
-  });
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-    reset,
-  } = useForm({
-    defaultValues: {
-      email: "",
-      subject: "",
-      message: "",
-    },
-    resolver: yupResolver(validationSchema),
-  });
-
-  const handleOnSubmit = async (event: any) => {
-    emailJs.init(publicKey);
-    console.log(event);
-    const { email, subject, message } = event;
-    const response = await emailJs.send(serviceId, templateId, {
-      from_name: email,
-      subject,
-      message,
+    const {enqueueSnackbar} = useSnackbar();
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .email(`L'adresse email est invalide`)
+            .required(`L'adresse email est requise`),
+        subject: Yup.string().required(`Le sujet est requis`),
+        message: Yup.string().required(`Le message est requis`),
     });
-    if (response.status === 200) {
-      enqueueSnackbar("Email sent successfully", { variant: "success" });
-      reset({
-        email: "",
-        subject: "",
-        message: "",
-      });
-    } else {
-      enqueueSnackbar("Email failed to send", { variant: "error" });
-    }
-  };
-  return (
-    <Container>
-      <Toolbar />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          mt: 10,
-        }}
-      >
-        <FormControl
-          component={"form"}
-          sx={{
-            sm: { width: "100%" },
-            xs: { width: "100%" },
-            md: { width: "50%" },
-            lg: { width: "50%" },
-            xl: { width: "50%" },
-          }}
-          onSubmit={handleSubmit(handleOnSubmit)}
-        >
-          <Paper
-            elevation={12}
-            sx={{ p: 5, display: "flex", flexDirection: "column" }}
-          >
-            <Typography
-              align={"center"}
-              variant={"h3"}
-              color={"primary"}
-              sx={{ my: 5 }}
+    const {
+        handleSubmit,
+        register,
+        formState: {errors},
+        reset,
+    } = useForm({
+        defaultValues: {
+            email: "",
+            subject: "",
+            message: "",
+        },
+        resolver: yupResolver(validationSchema),
+    });
+
+    const handleOnSubmit = async (event: any) => {
+        emailJs.init(publicKey);
+        console.log(event);
+        const {email, subject, message} = event;
+        const response = await emailJs.send(serviceId, templateId, {
+            from_name: email,
+            subject,
+            message,
+        });
+        if (response.status === 200) {
+            enqueueSnackbar("Email sent successfully", {variant: "success"});
+            reset({
+                email: "",
+                subject: "",
+                message: "",
+            });
+        } else {
+            enqueueSnackbar("Email failed to send", {variant: "error"});
+        }
+    };
+    return (
+        <Container>
+            <Toolbar/>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                    mt: 10,
+                }}
             >
-              Send me an e-mail !
-            </Typography>
-            <TextField
-              label={"Email"}
-              sx={{ mb: 3 }}
-              error={Boolean(errors.email)}
-              helperText={errors.email?.message}
-              autoComplete={"email"}
-              {...register("email", { required: true })}
-            ></TextField>
-            <TextField
-              label={"Subject"}
-              sx={{ mb: 3 }}
-              error={Boolean(errors.subject)}
-              helperText={errors.subject?.message}
-              {...register("subject", { required: true })}
-            ></TextField>
-            <TextField
-              label={"Message"}
-              sx={{ mb: 3 }}
-              multiline
-              rows={4}
-              error={Boolean(errors.message)}
-              helperText={errors.message?.message}
-              {...register("message", { required: true })}
-            ></TextField>
-            <Button variant={"contained"} sx={{ p: 2 }} type={"submit"}>
-              Send Mail
-            </Button>
-          </Paper>
-        </FormControl>
-      </Box>
-    </Container>
-  );
+                <FormControl
+                    component={"form"}
+                    sx={{
+                        sm: {width: "100%"},
+                        xs: {width: "100%"},
+                        md: {width: "50%"},
+                        lg: {width: "50%"},
+                        xl: {width: "50%"},
+                    }}
+                    onSubmit={handleSubmit(handleOnSubmit)}
+                >
+                    <Paper
+                        elevation={12}
+                        sx={{p: 5, display: "flex", flexDirection: "column"}}
+                    >
+                        <Typography
+                            align={"center"}
+                            variant={"h3"}
+                            color={"primary"}
+                            sx={{my: 5}}
+                        >
+                            Send me an e-mail !
+                        </Typography>
+                        <TextField
+                            label={"Email"}
+                            sx={{mb: 3}}
+                            error={Boolean(errors.email)}
+                            helperText={errors.email?.message}
+                            autoComplete={"email"}
+                            {...register("email", {required: true})}
+                        ></TextField>
+                        <TextField
+                            label={"Subject"}
+                            sx={{mb: 3}}
+                            error={Boolean(errors.subject)}
+                            helperText={errors.subject?.message}
+                            {...register("subject", {required: true})}
+                        ></TextField>
+                        <TextField
+                            label={"Message"}
+                            sx={{mb: 3}}
+                            multiline
+                            rows={4}
+                            error={Boolean(errors.message)}
+                            helperText={errors.message?.message}
+                            {...register("message", {required: true})}
+                        ></TextField>
+                        <Button variant={"contained"} sx={{p: 2}} type={"submit"}>
+                            Send Mail
+                        </Button>
+                    </Paper>
+                </FormControl>
+            </Box>
+        </Container>
+    );
 };
 
 export default Contact;
--- src/pages/Hobbies.tsx
+++ src/pages/Hobbies.tsx
@@ -1,101 +1,113 @@
-import {
-  Box,
-  Divider,
-  Grid,
-  IconButton,
-  Paper,
-  Stack,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from "@mui/material";
-import { GitHub, LinkedIn } from "@mui/icons-material";
+import {Box, Divider, Grid, IconButton, Paper, Stack, Toolbar, Tooltip, Typography,} from "@mui/material";
+import {AssignmentInd, GitHub, LinkedIn} from "@mui/icons-material";
 import SkillsComponent from "../components/SkillsComponent";
+// @ts-ignore
+import CV from "../assets/pdf/CV Anthony Limon.pdf"
 
 const Hobbies = () => {
-  return (
-    <Box>
-      <Toolbar />
-      <Box
-        sx={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          marginTop: 10,
-        }}
-      >
-        <Stack width={"100%"}>
-          <Box mb={5}>
-            <Divider variant={"middle"} />
-            <Typography
-              align={"center"}
-              variant={"h2"}
-              color={"primary"}
-              sx={{ my: 2 }}
+    const handleOnClick = async () => {
+        const element = await fetch(CV)
+        const blob = await element.blob()
+        const url = URL.createObjectURL(blob)
+        let alink = document.createElement("a")
+        alink.href = url
+        alink.download = CV
+        alink.click()
+    }
+
+    return (
+        <Box>
+            <Toolbar/>
+            <Box
+                sx={{
+                    height: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: 10,
+                }}
             >
-              About me :
-            </Typography>
-            <Divider variant={"middle"} />
-          </Box>
-          <Grid container spacing={2} mt={5}>
-            <Grid item xs={12} md={6}>
-              <Paper elevation={12} sx={{ p: 5 }}>
-                <Box display={"flex"} flexWrap={"wrap"}>
-                  <Typography variant={"h5"} color={"primary"} mr={2}>
-                    Email :
-                  </Typography>
-                  <Typography variant={"h5"} flexWrap={"wrap"}>
-                    anthony.limon@epitech.eu
-                  </Typography>
-                </Box>
-                <Box display={"flex"} flexWrap={"wrap"} my={4}>
-                  <Typography variant={"h5"} color={"primary"} mr={2}>
-                    Phone :
-                  </Typography>
-                  <Typography variant={"h5"}>07 60 06 05 74</Typography>
-                </Box>
-                <Box display={"flex"} my={4} flexWrap={"wrap"}>
-                  <Typography variant={"h5"} color={"primary"} mr={2}>
-                    Location :
-                  </Typography>
-                  <Typography variant={"h5"}>Bollwiller, Alsace</Typography>
-                </Box>
-                <Box display={"flex"} my={4} flexWrap={"wrap"}>
-                  <Typography variant={"h5"} color={"primary"} mr={2}>
-                    Date of birth :
-                  </Typography>
-                  <Typography variant={"h5"}>16/08/1997</Typography>
-                </Box>
-                <Box display={"flex"} flexWrap={"wrap"}>
-                  <Tooltip title={"My Github"} placement={"bottom"} arrow>
-                    <IconButton
-                      aria-label={"My Github"}
-                      href={"https://github.com/anthonynomil"}
-                      size={"large"}
-                    >
-                      <GitHub />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title={"My Linkedin"} placement={"bottom"} arrow>
-                    <IconButton
-                      aria-label={"My Linkedin"}
-                      href={"https://www.linkedin.com/in/limon-anthony/"}
-                      size={"large"}
-                    >
-                      <LinkedIn />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <SkillsComponent />
-            </Grid>
-          </Grid>
-        </Stack>
-      </Box>
-    </Box>
-  );
+                <Stack width={"100%"}>
+                    <Box mb={5}>
+                        <Divider variant={"middle"}/>
+                        <Typography
+                            align={"center"}
+                            variant={"h2"}
+                            color={"primary"}
+                            sx={{my: 2}}
+                        >
+                            About me :
+                        </Typography>
+                        <Divider variant={"middle"}/>
+                    </Box>
+                    <Grid container spacing={2} mt={5}>
+                        <Grid item xs={12} md={6}>
+                            <Paper elevation={12} sx={{p: 5}}>
+                                <Box display={"flex"} flexWrap={"wrap"}>
+                                    <Typography variant={"h5"} color={"primary"} mr={2}>
+                                        Email :
+                                    </Typography>
+                                    <Typography variant={"h5"} flexWrap={"wrap"}>
+                                        anthony.limon@epitech.eu
+                                    </Typography>
+                                </Box>
+                                <Box display={"flex"} flexWrap={"wrap"} my={4}>
+                                    <Typography variant={"h5"} color={"primary"} mr={2}>
+                                        Phone :
+                                    </Typography>
+                                    <Typography variant={"h5"}>07 60 06 05 74</Typography>
+                                </Box>
+                                <Box display={"flex"} my={4} flexWrap={"wrap"}>
+                                    <Typography variant={"h5"} color={"primary"} mr={2}>
+                                        Location :
+                                    </Typography>
+                                    <Typography variant={"h5"}>Bollwiller, Alsace</Typography>
+                                </Box>
+                                <Box display={"flex"} my={4} flexWrap={"wrap"}>
+                                    <Typography variant={"h5"} color={"primary"} mr={2}>
+                                        Date of birth :
+                                    </Typography>
+                                    <Typography variant={"h5"}>16/08/1997</Typography>
+                                </Box>
+                                <Box display={"flex"} flexWrap={"wrap"}>
+                                    <Tooltip title={"My Github"} placement={"bottom"} arrow>
+                                        <IconButton
+                                            aria-label={"My Github"}
+                                            href={"https://github.com/anthonynomil"}
+                                            size={"large"}
+                                        >
+                                            <GitHub/>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title={"My Linkedin"} placement={"bottom"} arrow>
+                                        <IconButton
+                                            aria-label={"My Linkedin"}
+                                            href={"https://www.linkedin.com/in/limon-anthony/"}
+                                            size={"large"}
+                                        >
+                                            <LinkedIn/>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title={"My CV"} placement={"bottom"} arrow>
+                                        <IconButton
+                                            arial-label={"My CV"}
+                                            href={"#"}
+                                            size={"large"}
+                                            onClick={handleOnClick}
+                                        >
+                                            <AssignmentInd/>
+                                        </IconButton>
+                                    </Tooltip>
+                                </Box>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <SkillsComponent/>
+                        </Grid>
+                    </Grid>
+                </Stack>
+            </Box>
+        </Box>
+    );
 };
 
 export default Hobbies;
